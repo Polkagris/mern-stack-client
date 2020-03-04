eb5da86018bad03243684e63bdcdc7252c941c2f
unknown <arne.n.bjerkaas@gmail.com>
2020-03-04 21:09:19 +0100

    Add duration input

--- src/container/Create.js
+++ src/container/Create.js
@@ -1,66 +1,77 @@
-import React, { useState, useEffect } from 'react'
-import { Form } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { Form } from "react-bootstrap";
 import "react-datepicker/dist/react-datepicker.css";
-import axios from 'axios';
+import axios from "axios";
 
 function CreateExercise(props) {
-    const [description, setDescription] = useState("");
-    const [token, setToken] = useState("");
+  const [description, setDescription] = useState("");
+  const [token, setToken] = useState("");
+  const [duration, setDuration] = useState("");
+  //const [date, setDate] = useState(new Date());
 
-    const getUserInfo = async () => {
-        const testToken = localStorage.getItem('token');
-        setToken(testToken);
-    }
+  const getUserInfo = async () => {
+    const testToken = localStorage.getItem("token");
+    setToken(testToken);
+  };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  const handleSubmit = async event => {
+    event.preventDefault();
 
-        try {
-            const response = await axios.post(`http://localhost:5000/users/exercise/`,
-                {
-                    token: token,
-                    description: description
-                },
-                {
-                    headers:
-                    {
-                        Authorization: token
-                    }
-                });
-            const data = await response.data;
-            console.log("Data from create exercise:", data);
-
-        } catch (err) {
-            console.log("Error posting:", err);
+    try {
+      const response = await axios.post(
+        `http://localhost:5000/users/exercise/`,
+        {
+          token: token,
+          description: description,
+          duration: duration
+          //date: date
+        },
+        {
+          headers: {
+            Authorization: token
+          }
         }
-        props.history.push("/");
+      );
+      const data = await response.data;
+      console.log("Data from create exercise:", data);
+    } catch (err) {
+      console.log("Error posting:", err);
     }
+    props.history.push("/");
+  };
 
-    const handleDescriptionChange = (event) => {
-        setDescription(event.target.value);
-    }
+  const handleDescriptionChange = event => {
+    setDescription(event.target.value);
+  };
 
+  useEffect(() => {
+    getUserInfo();
+  }, [token]);
 
-    useEffect(() => {
-        getUserInfo();
-    }, [token]);
-
-    return (
-        <div>
-            <h1>This is the page where you create a new exercise. Just saying...</h1>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Exercise description</Form.Label>
-                    <Form.Control
-                        onChange={handleDescriptionChange}
-                        placeholder="description"
-                        type="text"
-                        value={description} />
-                </Form.Group>
-                <input type="submit" value="Submit" className="btn btn-primary"></input>
-            </Form>
-        </div>
-    )
+  return (
+    <div>
+      <h1>This is the page where you create a new exercise. Just saying...</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Exercise description</Form.Label>
+          <Form.Control
+            onChange={handleDescriptionChange}
+            placeholder="description"
+            type="text"
+            value={description}
+          />
+          <Form.Label>Exercise duration</Form.Label>
+          <Form.Control
+            onChange={event => setDuration(event.target.value)}
+            placeholder="duration"
+            type="text"
+            value={duration}
+          />
+        </Form.Group>
+        <input type="submit" value="Submit" className="btn btn-primary"></input>
+      </Form>
+    </div>
+  );
 }
 
 export default CreateExercise;
--- src/container/Home.js
+++ src/container/Home.js
@@ -1,72 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import Table from 'react-bootstrap/Table';
-import jwt from 'jsonwebtoken';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import jwt from "jsonwebtoken";
+import axios from "axios";
 //import 'jwt-decode';
 
 function Home() {
-    const [fetchedExercises, setFetchedExercises] = useState([]);
-    const [token, setToken] = useState("");
+  const [fetchedExercises, setFetchedExercises] = useState([]);
+  const [token, setToken] = useState("");
 
-    const getUserInfo = async () => {
-        const testToken = localStorage.getItem('token');
-        setToken(testToken);
-    }
+  const getUserInfo = async () => {
+    const testToken = localStorage.getItem("token");
+    setToken(testToken);
+  };
 
-    const getExerciseList = async () => {
-        if (token === "") return;
-        const response = await axios.post(`http://localhost:5000/training/`,
-            { token: token },
-            {
-                headers:
-                {
-                    Authorization: token
-                }
-            });
-        const data = response.data;
-        setFetchedExercises(data);
-        return data;
-    }
+  const getExerciseList = async () => {
+    if (token === "") return;
+    const response = await axios.post(
+      `http://localhost:5000/training/`,
+      { token: token },
+      {
+        headers: {
+          Authorization: token
+        }
+      }
+    );
+    const data = response.data;
+    setFetchedExercises(data);
+    return data;
+  };
 
-    // This runs twice, one time at render, 
-    // second time when token is updated.
-    useEffect(() => {
-        getUserInfo();
-        getExerciseList();
-    }, [token]);
+  // This runs twice, one time at render,
+  // second time when token is updated.
+  useEffect(() => {
+    getUserInfo();
+    getExerciseList();
+  }, [token]);
 
+  return (
+    <div>
+      <h1>This is the home page. Welcome!</h1>
 
+      <Table responsive striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>Exercise</th>
+          </tr>
+        </thead>
 
-
-    return (
-        <div>
-            <h1>
-                This is the home page. Welcome!
-        </h1>
-
-            <Table responsive striped bordered hover variant="dark">
-                <thead>
-                    <tr>
-                        <th>Exercise</th>
-                    </tr>
-                </thead>
-
-                {/* Check for undefined */}
-                {fetchedExercises.length < 1 ?
-                    <div>Loading...</div>
-                    :
-                    fetchedExercises.exercises.map(exercise => (
-                        <tbody key={exercise._id}>
-                            <tr>
-                                <td>{exercise.description}</td>
-                            </tr>
-                        </tbody>
-                    ))
-                }
-
-            </Table>
-        </div>
-    )
+        {/* Check for undefined */}
+        {fetchedExercises.length < 1 ? (
+          <div>Loading...</div>
+        ) : (
+          fetchedExercises.exercises.map(exercise => (
+            <tbody key={exercise._id}>
+              <tr>
+                <td>{exercise.description}</td>
+              </tr>
+              <tr>
+                <td>{exercise.duration}</td>
+              </tr>
+            </tbody>
+          ))
+        )}
+      </Table>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
